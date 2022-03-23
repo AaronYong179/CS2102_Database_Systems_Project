8334b93ba62945406ff7f15131202366e4f30c8f
AaronYong179 <54031303+AaronYong179@users.noreply.github.com>
2022-03-23 13:27:01 +0800

    Create seed_database.sql

---
+++ seed_database.sql
@@ -0,0 +1,112 @@
+--------------
+-- EMPLOYEE --
+--------------
+CALL add_employee('Tanner Cantrell', '59573 Dustin Row Cookside, AL 07450', '43934212', 'tannercantrell@inlook.com', 4457.1, 'full-time', '2007-07-07', 'administrator');
+CALL add_employee('Brendan Garcia', '3120 Chambers Knoll Suite 654 New Michael, VA 55221', '61070293', 'brendangarcia@inlook.com', 5558.15, 'full-time', '2014-08-15', 'administrator');
+CALL add_employee('Karen Johnson', '0944 Gene Lane Suite 437 New Robert, VT 59635', '30263126', 'karenjohnson@inlook.com', 4372.91, 'full-time', '2020-09-01', 'administrator');
+CALL add_employee('Stacy Ellison', 'Unit 1638 Box 5476 DPO AP 46817', '35557953', 'stacyellison@inlook.com', 5768.39, 'full-time', '1979-03-25', 'administrator');
+CALL add_employee('Patrick Zimmerman', '6105 Barrera Expressway Apt. 659 South Timothy, SD 27938', '16506878', 'patrickzimmerman@inlook.com', 4384.2, 'full-time', '1997-05-08', 'administrator');
+CALL add_employee('Scott Ruiz', '75276 James Estate Apt. 705 East Angela, NC 26698', '06172028', 'scottruiz@inlook.com', 4045.34, 'full-time', '1996-07-21', 'administrator');
+CALL add_employee('Cassandra Jones', 'Unit 2968 Box 0808 DPO AE 14772', '87644201', 'cassandrajones@inlook.com', 4367.24, 'full-time', '2016-11-20', 'administrator');
+CALL add_employee('Tasha Perez', '745 Daugherty Plains Mackville, NY 51066', '73126549', 'tashaperez@inlook.com', 5303.45, 'full-time', '2006-09-28', 'administrator');
+CALL add_employee('Matthew Cunningham', '9896 Kelly Parkways Suite 044 Hawkinston, DC 93130', '75227581', 'matthewcunningham@inlook.com', 4757.39, 'full-time', '1982-09-13', 'administrator');
+CALL add_employee('Heather Rivera', '5553 Brian Meadows Apt. 032 Thompsonville, FL 15427', '60080648', 'heatherrivera@inlook.com', 5954.17, 'full-time', '1972-09-08', 'administrator');
+CALL add_employee('Matthew Cox', '171 Kevin Shoals Apt. 690 Riceport, RI 64619', '75153384', 'matthewcox@inlook.com', 4696.48, 'full-time', '1985-12-22', 'manager', '{Programming Methodology}');
+CALL add_employee('Michelle Mccormick', '162 Alexander Stravenue Suite 299 Lake Brian, IN 89829', '47948863', 'michellemccormick@inlook.com', 5999.03, 'full-time', '2011-07-21', 'manager', '{Data Structures and Algorithms}');
+CALL add_employee('Nicole Hernandez', '825 Buchanan Wall West Eric, CA 44074', '66423603', 'nicolehernandez@inlook.com', 4644.49, 'full-time', '2017-01-14', 'manager', '{Computer Organisation}');
+CALL add_employee('Michael Robertson', '940 Rebecca Landing Apt. 842 Camposshire, LA 32668', '27993439', 'michaelrobertson@inlook.com', 4632.26, 'full-time', '1990-04-16', 'manager', '{Database Systems}');
+CALL add_employee('Thomas Solomon', '46580 Brandon Corner Suite 008 West Rachel, SC 34574', '89786985', 'thomassolomon@inlook.com', 4980.49, 'full-time', '2000-02-07', 'manager', '{Computer Networks}');
+CALL add_employee('Carl Sparks', '107 James View Shawnstad, MA 97489', '84167820', 'carlsparks@inlook.com', 4588.6, 'full-time', '1987-07-22', 'manager', '{Information Security}');
+CALL add_employee('Ashley Carter', '050 Lopez Spring South Meagan, KS 68458', '77493886', 'ashleycarter@inlook.com', 4510.19, 'full-time', '1993-03-23', 'manager', '{Computational Biology}');
+CALL add_employee('Charles Torres', '63755 Ashley Skyway Lake Kevin, RI 41661', '48931880', 'charlestorres@inlook.com', 4575.08, 'full-time', '2005-01-03', 'manager', '{Operating Systems}');
+CALL add_employee('Michelle Benson', '5232 Myers Rest West Sarah, TN 65490', '87062065', 'michellebenson@inlook.com', 5324.61, 'full-time', '1996-03-13', 'manager', '{Media Computing}');
+CALL add_employee('Jessica Lee', '2396 Murphy Squares Suite 064 New Aaronburgh, UT 56501', '77533447', 'jessicalee@inlook.com', 5988.7, 'full-time', '1972-05-03', 'manager', '{Computer Architecture, Computer Graphics, Artificial Intelligence}');
+CALL add_employee('Brian Davis', '328 Rodriguez Island Whiteborough, CT 99500', '27664878', 'briandavis@inlook.com', 5419.32, 'full-time', '2013-11-08', 'instructor', '{Programming Methodology}');
+CALL add_employee('Andrew Lopez', '520 Jeffrey Mills Apt. 223 Bethview, MD 83887', '92036531', 'andrewlopez@inlook.com', 18.96, 'part-time', '2012-02-26', 'instructor', '{Data Structures and Algorithms}');
+CALL add_employee('Sarah Bernard', '583 Ashley Junctions Port Erica, ME 58768', '78305413', 'sarahbernard@inlook.com', 4505.66, 'full-time', '2010-11-05', 'instructor', '{Computer Organisation}');
+CALL add_employee('Cheryl Bray', '4090 Larson Plaza Ralphville, VA 90827', '74486447', 'cherylbray@inlook.com', 7.71, 'part-time', '2016-06-05', 'instructor', '{Database Systems}');
+CALL add_employee('Joshua Webb', '19914 Steven Circles Donaldville, VT 62199', '95732170', 'joshuawebb@inlook.com', 4137.29, 'full-time', '2009-02-12', 'instructor', '{Computer Networks}');
+CALL add_employee('James Mathis MD', '9541 Levine Burgs Apt. 753 Port Connor, SC 37977', '25165477', 'jamesmathismd@inlook.com', 38.61, 'part-time', '1996-01-01', 'instructor', '{Information Security}');
+CALL add_employee('Brittany Cobb', '11580 Jones Tunnel Suite 567 South Brian, MI 42068', '36921120', 'brittanycobb@inlook.com', 5028.23, 'full-time', '1989-01-06', 'instructor', '{Computational Biology}');
+CALL add_employee('Patrick Anderson', '75269 Murphy Trace Robinsonstad, TN 65394', '31864963', 'patrickanderson@inlook.com', 35.42, 'part-time', '2007-12-29', 'instructor', '{Operating Systems}');
+CALL add_employee('Gregory Huynh', '678 Daniel Shores East Ronnie, KY 33307', '50286376', 'gregoryhuynh@inlook.com', 5870.8, 'full-time', '2018-12-06', 'instructor', '{Media Computing}');
+CALL add_employee('Kathleen Aguirre', '16235 Lisa Courts Pattersonshire, NY 77134', '48655894', 'kathleenaguirre@inlook.com', 35.82, 'part-time', '1975-05-28', 'instructor', '{Computer Architecture, Computer Graphics, Artificial Intelligence}');
+CALL add_employee('Kimberly Miles', '415 Cooper Turnpike Suite 718 South Ronald, ME 24211', '60526731', 'kimberlymiles@inlook.com', 5533.81, 'full-time', '1970-07-13', 'instructor', '{Programming Methodology}');
+CALL add_employee('Taylor Brown DDS', '033 Smith Shoal Suite 323 Kyleborough, GA 22620', '77961186', 'taylorbrowndds@inlook.com', 27.49, 'part-time', '2001-06-16', 'instructor', '{Data Structures and Algorithms}');
+CALL add_employee('Christina Adams', '905 Campbell Light South Cherylshire, MA 30054', '11005583', 'christinaadams@inlook.com', 4255.77, 'full-time', '2000-10-14', 'instructor', '{Computer Organisation}');
+CALL add_employee('Tonya Zimmerman', '81460 Craig Ports Johnborough, NE 36487', '26730235', 'tonyazimmerman@inlook.com', 21.24, 'part-time', '1977-02-15', 'instructor', '{Database Systems}');
+CALL add_employee('Tony Meyers', '92645 Fleming Loop Danielberg, DE 59751', '00167482', 'tonymeyers@inlook.com', 4683.69, 'full-time', '1990-03-20', 'instructor', '{Computer Networks}');
+CALL add_employee('Chris Solis', '212 Boone Island Apt. 063 Clarkland, MN 80046', '69068832', 'chrissolis@inlook.com', 31.66, 'part-time', '2011-03-16', 'instructor', '{Information Security}');
+CALL add_employee('Brittany Mendez', '5146 Daniel Flats Calvinberg, NY 26606', '45837385', 'brittanymendez@inlook.com', 5305.11, 'full-time', '1991-06-07', 'instructor', '{Computational Biology}');
+CALL add_employee('Robert Walker', '2387 Smith Overpass New Christine, RI 77801', '26705259', 'robertwalker@inlook.com', 38.49, 'part-time', '1994-09-10', 'instructor', '{Operating Systems}');
+CALL add_employee('Luis Carr', '7656 Shannon Extensions Lake Jasonfurt, OH 89761', '72340283', 'luiscarr@inlook.com', 4953.65, 'full-time', '1995-12-17', 'instructor', '{Media Computing}');
+CALL add_employee('Michael Mitchell', '6505 Young Street New Amyfort, DC 34585', '94573591', 'michaelmitchell@inlook.com', 13.73, 'part-time', '1972-08-04', 'instructor', '{Computer Architecture, Computer Graphics, Artificial Intelligence}');
+
+
+--------------
+-- CUSTOMER --
+--------------
+CALL add_customer('Leah Ross', '2 Gill spurs New Teresa L74 2ZT', '08408144', 'leahross@customers.com', '8062458755758821', '12-2039', '212');
+CALL add_customer('Margaret Barton-Morris', '225 Jemma landing Lake Garytown S7 6HX', '51843344', 'margaretbarton-morris@customers.com', '9152354343169068', '09-2025', '695');
+CALL add_customer('Conor Thomas', 'Flat 18v William pines Davidberg N3A 3ZB', '48092624', 'conorthomas@customers.com', '9441741825470444', '06-2032', '725');
+CALL add_customer('Miss Ann Fletcher', '42 Jack crest Melanieton LA7 0UE', '10780140', 'missannfletcher@customers.com', '3417462936342402', '05-2030', '433');
+CALL add_customer('Dr. Natasha Williams', 'Flat 4 Richards terrace Vincentmouth M43 1ER', '82904630', 'dr.natashawilliams@customers.com', '1247666155249277', '11-2045', '093');
+CALL add_customer('Arthur Cooper', 'Flat 8 Malcolm corner Alanbury G04 1RE', '48550351', 'arthurcooper@customers.com', '0823255018486641', '10-2047', '153');
+CALL add_customer('Dr. Nicola Bowen', 'Flat 6 Mitchell ranch Lynneberg E8S 7EH', '87799856', 'dr.nicolabowen@customers.com', '7577483149899702', '05-2042', '508');
+CALL add_customer('Ms. Julie Walters', '291 Carolyn locks East Stewart KW7R 5TZ', '71166706', 'ms.juliewalters@customers.com', '4301227385413127', '07-2028', '730');
+CALL add_customer('Lucy Burgess', '944 Kate burgs South Katie E63 7PZ', '46467444', 'lucyburgess@customers.com', '4960673906952199', '03-2033', '428');
+CALL add_customer('Joyce Jackson', '694 Hill pike Payneville M27 0RJ', '77146883', 'joycejackson@customers.com', '1204722214999106', '04-2050', '004');
+
+-------------
+-- COURSES --
+-------------
+CALL add_course('Functional Abstraction','This course teaches functional abstraction in which details of the algorithms to accomplish the function are not visible to the consumer of the function. The consumer of the function need to only know the correct calling convention and have trust in the accuracy of the functional results.','Programming Methodology', 1);
+CALL add_course('Higher-Order Functions','This course introduces higher-order functions, a function that does at least one of the following: takes one or more functions as arguments (i.e. procedural parameters).','Programming Methodology', 3);
+CALL add_course('Sorting and Searching Algorithms','This course will provide the basic knowledge & understanding  of Sorting & Searching Algorithms.','Data Structures and Algorithms', 1);
+CALL add_course('Basic Analysis of Algorithms','This course teaches a calculus that enables precise quantitative predictions of large combinatorial structures. In addition, this course covers generating functions and real asymptotics and then introduces the symbolic method in the context of applications in the analysis of algorithms and basic structures such as permutations, trees, strings, words, and mappings.','Data Structures and Algorithms', 3);
+CALL add_course('Assembly Language','This course is designed to teach Assembly a low-level programming language for a computer or other programmable device specific to a particular computer architecture in contrast to most high-level programming languages which are generally portable across multiple systems.','Computer Organisation', 2);
+CALL add_course('Entity-Relationship Mode','The aim of this course is to introduce the fundamental concepts and techniques necessary for the understanding and practice of design and implementation of database applications and of the management of data with relational database management systems. The course theoretical aspects of design with the entity-relationship model.','Database Systems', 2);
+CALL add_course('Network Security','In this course you will examine the various areas of network security including intrusion detection evidence collection and defense against cyber attacks.','Computer Networks', 1);
+CALL add_course('Cryptosystems','This course introduces a cryptosystem, a suite of cryptographic algorithms needed to implement a particular security service most commonly for achieving confidentiality (encryption).','Information Security', 1);
+CALL add_course('Introduction to Bioinformatics','The course provides from programmers’ viewpoint an overview of common computational techniques used in the field of bioinformatics.','Computational Biology', 1);
+CALL add_course('Introduction to Operating Systems','This course introduces the basic concepts in operating systems and links it with contemporary operating systems (eg. Unix/Linux and Windows). It focuses on OS structuring and architecture, processes, memory management, concurrency and file systems.','Operating Systems', 3);
+CALL add_course('Introduction to Media Computing','This course introduces students to (i) the fundamental principles, theory, algorithms, and data structures behind digital representation, compression, synchronization, and processing of image, audio, and video data types, and (ii) challenges and issues in developing media-rich applications, such as media streaming and media retrieval.','Media Computing', 1);
+CALL add_course('RISC and vector computer','The objective of this course is concerned with design techniques involving the use of parallelism to improve the performance of computer systems. This course considers the fundamental methods to improve the performance of single processor systems.','Computer Architecture', 1);
+CALL add_course('Multi-processor systems I','The objective of this course is concerned with design techniques involving the use of parallelism to improve the performance of computer systems. This course examines in detail multi-processor systems using shared memory.','Computer Architecture', 2);
+CALL add_course('Introduction to Computer Graphics','This course teaches some graphics hardware devices,  reviews the mathematics related to the understanding, and discusses the fundamental areas of computer graphics.','Computer Graphics', 3);
+CALL add_course('Adversarial search','The course introduces the basic concepts in search and knowledge representation, focusing on adverserial search algorithms such as minimax and alpha-beta procedures.','Artificial Intelligence', 2);
+CALL add_course('First-order Logic','The course introduces the basic concepts in search and knowledge representation, focusing on first-order logic and resolution refutation.','Artificial Intelligence', 1);
+
+------------
+-- VENUES --
+------------
+INSERT INTO Venues VALUES
+('#01-01', 1, 1, 5),
+('#01-02', 1, 2, 10),
+('#01-03', 1, 3, 10),
+('#01-04', 1, 4, 10),
+('#01-05', 1, 5, 15),
+('#02-01', 2, 1, 5),
+('#02-02', 2, 2, 10),
+('#02-03', 2, 3, 10),
+('#02-04', 2, 4, 10),
+('#02-05', 2, 5, 15);
+
+---------------------
+-- COURSE_PACKAGES --
+---------------------
+CALL add_course_package('Welcome Gift', 1, '1-1-2000', NULL, 0);
+CALL add_course_package('Christmas Special', 5, '25-12-2012', '06-01-2013', 10.00);
+CALL add_course_package('Christmas Special 2021', 5, '25-12-2021', '06-01-2022', 10.00);
+CALL add_course_package('20-year Anniversary Thank You Gift', 3, '01-04-2020', '31-12-2020', 10.00);
+CALL add_course_package('10-Session Bundle', 10, '01-01-2021', '31-12-2021', 25.99);
+CALL add_course_package('15-Session Bundle', 15, '01-01-2021', '31-12-2021', 29.99);
+CALL add_course_package('25-Session Bundle', 25, '01-01-2021', '31-12-2021', 38.00);
+CALL add_course_package('Gift Basket Promotion', 3, '01-06-2015', NULL, 12.99);
+CALL add_course_package('11-11 Sale', 11, '11-11-2021', '12-11-2021', 11.11);
+CALL add_course_package('Summer School Package', 5, '01-03-2021', '01-05-2021', 10.00);
+
+---------------
+-- PAY_SLIPS --
+---------------
+SELECT * FROM pay_salary();
